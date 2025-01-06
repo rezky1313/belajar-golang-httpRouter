2b9342420b922ea41b99c507f61680ef4ac5f97e
rezky1313 <rezkywahyudi1313@gmail.com>
2025-01-06 23:23:02 +0700

    Pengenalan http Router

---
+++ 1. Pengenalan HTTP Router_test.go
@@ -0,0 +1,9 @@
+package belajar_golang_httpRouter
+
+/*
+HTTP Router
+-dikarenakan pada library http handler bawaan pada goalng memiliki keterbatasan maka disaran kan menggunakan library
+tambahan, salah satunya httprouter
+-merupakan salah satu library open suorce untuk fitur routing saja pada golang, terkenal dengan kecepatannya dan minimalis
+-https://github.com/julienschmidt/httprouter
+*/
